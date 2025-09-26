62a036413f0bdb0f029a645ed680e06fa4777aab
VI0LENTINE <W0516070@campus.nscc.ca>
2025-09-26 20:12:19 -0300

    Sprint 1 Changes Made

--- Models/Performance.cs
+++ Models/Performance.cs
@@ -5,10 +5,11 @@
         public int PerformanceId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
         public DateTime PerformanceDate { get; set; }
         public DateTime EndDate { get; set; }
 
-
+        public int CategoryId { get; set; }
         public Category? Category { get; set; }
     }
 }
