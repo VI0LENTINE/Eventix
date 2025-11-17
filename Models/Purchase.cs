8cf7c43575e1f0c15005e93870adefeebf87d401
VI0LENTINE <W0516070@campus.nscc.ca>
2025-11-17 16:20:43 -0400

    Saving progress before dropping DB

---
+++ Models/Purchase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eventix.Models
+{
+    public class Purchase
+    {   
+        public int PurchaseId { get; set; }
+        public int Tickets { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string CardLast4 {  get; set; } = string.Empty;
+        public DateTime PurchaseDate { get; set; }
+
+        // Foreign key
+        [Display(Name = "Id")]
+        public int PerformanceId { get; set; }
+
+        // Navigation property
+        public Performance? Performance { get; set; }
+
+    }
+}
